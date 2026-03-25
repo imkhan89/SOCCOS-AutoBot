3ab0925196fc9b1bdd22e59d6347308b3f63ef79
imkhan89 <info@ndestore.com>
2026-03-25 05:36:11 +0500

    Create categoryMenu.js

---
+++ interface/ui/menu/categoryMenu.js
@@ -0,0 +1,27 @@
+// interface/ui/menu/categoryMenu.js
+
+const { buildCTAGroup } = require("../components/cta");
+
+function categoryMenu() {
+  const message =
+`📦 *Browse Categories*
+
+Select a category to explore products:`;
+
+  const buttons = buildCTAGroup([
+    { id: "category_service_parts", title: "🔧 Service Parts" },
+    { id: "category_accessories", title: "🚘 Accessories" },
+    { id: "category_car_care", title: "🧼 Car Care" }
+  ]);
+
+  return {
+    type: "interactive",
+    message,
+    buttons,
+    meta: {
+      screen: "category_menu"
+    }
+  };
+}
+
+module.exports = categoryMenu;
